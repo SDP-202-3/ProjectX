111e67a339ea983c94cae092ebf8165812eb98e0
ПарусимовМ <@N201-2>
2021-06-25 17:17:46 +0600

    fyu

---
+++ Aleksey.h
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <ctime>
+#include <fstream>
+#include <vector>
+#include <Windows.h>
+#include <conio.h>
+using namespace std;
+
+
+class User {
+
+	string login;
+	string password;
+	int id;
+
+};
