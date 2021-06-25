ebd61145ebe739c4bda5c44fe937931520a73e08
ostrovsky nikita <n.saratovtsev@mail.ru>
2021-06-25 17:10:22 +0600

    15645

--- Source.cpp
+++ Source.cpp
@@ -12,9 +12,11 @@ int main()
 	srand(time(NULL));
 	cout << "Alburt-Alina" << endl;
 	cout << "Teacher" << endl;
-	cout << "Ruslan" << endl;
-	cout << "ГЫ" << endl;
 	cout << "жаренная картошка" << endl;
+	cout << "Ruslan" << endl;
+	cout << "Р“Р«" << endl;
+	cout << "Р¶Р°СЂРµРЅРЅР°СЏ РєР°СЂС‚РѕС€РєР°" << endl;
+	cout << "fried potatoes" << endl;
 	cout << "Vladimir " << endl;
 	cout << "Lesha" << endl;
 	cout << "Nikita" << endl;
@@ -22,9 +24,18 @@ int main()
 	cout << "Doshik" << endl;
 	cout << "Nikitaaaaaa" << endl;
 	cout << "Doshan" << endl;
+	cout << "Ilya" << endl;
 	cout << "Gyggygyygyg" << endl;																																						cout << "Ilya" << endl;
 	cout << "Victor" << endl;
 	cout << "Igor" << endl;
-	cout << "????? ????? ?????" << endl;
-
+	cout << "пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ" << endl;
+	cout << "t   House:nn";
+	cout << "t     / \n";
+	cout << "t    /   \n";
+	cout << "t   /     \n";
+	cout << "t  /------ \n";
+	cout << "t / |    |  \n";
+	cout << "t   |    | \n";
+	cout << "t   |    | \n";
+	cout << "t   |____| \n";
 }
