e855741119fe1f1ae1d320d8fcade7a492851fc7
ПарусимовМ <@N201-2>
2021-06-25 17:24:02 +0600

    dsf

--- Aleksey.h
+++ Aleksey.h
@@ -24,4 +24,21 @@ public:
 		cout << "name :" << name << endl;
 
 	}
+	string generatePwd(int n = 10) {
+		string newpwd;
+		string s = "qwertyuiopasdfghjklzxcvbnm1234567890QWERTYUIOPASDFGHJKLZXCVBNM";
+		for (int i = 0; i < n; i++) {
+			newpwd += s[rand() % s.length()];
+		}
+		return newpwd;
+	}
+
+	string generateID(int n = 20) {
+		string newID;
+		string s = "1234567890";
+		for (int i = 0; i < n; i++) {
+			newID += s[rand() % s.length()];
+		}
+		return newID;
+	}
 };
