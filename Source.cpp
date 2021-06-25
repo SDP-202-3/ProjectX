47390791802a48e11ba1a2e3402f2bb331b13f53
Дошан Есмаганбет <doshan.de@gmail.com>
2021-06-25 17:06:02 +0600

    Mark nadoel

--- Source.cpp
+++ Source.cpp
@@ -6,19 +6,19 @@
 #include <Windows.h>
 #include <conio.h>
 using namespace std;
-int main()
-{
+int main() {
 	SetConsoleCP(1251); SetConsoleOutputCP(1251);
 	system("color 02");
 	srand(time(NULL));
 	cout << "Alburt-Alina" << endl; 
 	cout << "Teacher" << endl;
-	cout << "fried potatoes" << endl;
+	cout << "fried potatoes (Mark, nadoel)" << endl;
 	cout << "Vladimir " << endl;
 	cout << "Алексей " << endl;
 	cout << "Nikitaaaaaa" << endl;
 	cout << "Doshan" << endl;
-	cout << "Gyggygyygyg" << endl;																																						cout << "Ilya" << endl;
+	cout << "Gyggygyygyg" << endl;																																						
+	cout << "Ilya" << endl;
 	cout << "Victor" << endl;
 	cout << "Igor" << endl;
 }
