91e3667717dae6f33e4d8da059e7cd3ed2b420e1
ПарусимовМ <@N201-2>
2021-06-25 17:20:31 +0600

    asd

--- Aleksey.h
+++ Aleksey.h
@@ -13,5 +13,15 @@ class User {
 	string login;
 	string password;
 	int id;
+	string name;
+public:
+	User(string login,string password, int id, string name): login(login),password(password),id(id),name(name){}
+	User(){}
+	void print() {
+		cout << "login :" << login << endl;
+		cout << "password :" << password << endl;
+		cout << "id :" << id << endl;
+		cout << "name :" << name << endl;
 
+	}
 };
