f3a1c3f596baa23b47a01a2d82b9c690e94b1630
СиротинИ <@N201-T>
2021-06-25 17:47:59 +0600

    ужос

--- igor.h
+++ igor.h
@@ -3,17 +3,8 @@
 #include <string>
 using namespace std;
 class User {
-private:
-	string login;
-	string pwd;
-	string email;
-	string phone;
-public:
-	User(string lg, string pwd, string email, string num) : login(lg), pwd(pwd), email(email), phone(num) {}
-	User(){}
-
-	void setData(string lg, string pwd, string email, string num) { this->login = lg; this->pwd = pwd; this->email = email; this->phone = num; }
-	string getPwd() { return pwd; }
-
+private: string login; string pwd; string email; string phone;
+public: User(string lg, string pwd, string email, string num) : login(lg), pwd(pwd), email(email), phone(num) {} User(){}
+	void setData(string lg, string pwd, string email, string num) { this->login = lg; this->pwd = pwd; this->email = email; this->phone = num; } string getPwd() { return pwd; }
 	void print() { cout << login << " | " << pwd << " | " << email << " | " << phone << endl; }
 };
